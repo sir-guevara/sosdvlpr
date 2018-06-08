137a0f03d3ba1bf3207f075c3ad2eab388b5c3cb
Groovy Guevara <groovyguev@gmail.com>
2018-06-08 22:39:30 +0100

    added ignore file

--- sosdvlpr/models.py
+++ sosdvlpr/models.py
@@ -4,11 +4,11 @@ from django.utils import timezone
 
 
 class Thread(models.Model):
-    is_sos = models.BooleanField(default=False)
     owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=60)
     image = models.URLField(blank=True, null=True)
     text = models.TextField()
+    is_sos = models.BooleanField(default=False)
     created_on =models.DateTimeField(default=timezone.now)
 
     def __str__(self):
@@ -19,4 +19,8 @@ class Comment(models.Model):
     owner = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     thread = models.ForeignKey('Thread', on_delete=models.CASCADE)
     text = models.TextField()
+    image = models.URLField(blank=True, null=True)
+
+    def __str__(self):
+        return self.text
     

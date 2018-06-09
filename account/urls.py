ca6f66c17fdb81b7d7b374015cda645011d28a30
Groovy Guevara <groovyguev@gmail.com>
2018-06-09 13:22:16 +0100

    adding Profiles

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 
@@ -7,3 +8,7 @@ urlpatterns =[
     path('',views.login, name='login'),
     path('register/', views.register, name='register'),
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
